4b415422246a72939ac0aed69e00c186af7b9ae2
Dev1fo <samy.liani@gmail.com>
2020-02-22 12:04:32 +0100

    seconde commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
-import TabsDefault from './components/TabsDefault';
-import FooterPage from './components/FooterPage'
+import TabsDefault from "./components/TabsDefault";
+import FooterPage from "./components/FooterPage";
 function App() {
   return (
     <div className="App">
-      
+      <h1>HElloooooooooooooooo les amisssssssss</h1>
+      <TabsDefault />
 
-      <TabsDefault/>
-
-      <FooterPage/>
-
-
-
-
-
-
-
-
-
-
-
+      <FooterPage />
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
